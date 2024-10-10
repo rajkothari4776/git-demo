153c1fa667e9efda6acbf2e66a912d54ce455272
rajkothari4776 <rajkothari4776@gmail.com>
2024-10-10 18:18:43 +0000

    add java file and js file

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-let n = 234;
-console.log(Math.floor(n/=10));
+// let n = 234;
+// console.log(Math.floor(n/=10));
+
+// let arr = [1,2,3,4,5];
+// let arr2 = arr.reverse();
+// console.log(arr2);  
+
+let firstName = "RajKothari";
+let len = firstName.length - 1;
+// console.log(len);
+
+let random = Math.round(Math.random() * len) ;
+// console.log(random);
+
+console.log(firstName.charAt(random));
+
+
